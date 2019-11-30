d899a065c47ace2238769ce54b8f8d7e44ecb41c
psomail <$Sergo05>
2019-11-30 17:32:51 +0300

    4

--- colorPallete/src/windows/RightPanel.java
+++ colorPallete/src/windows/RightPanel.java
@@ -5,11 +5,24 @@ import java.awt.*;
 
 public class RightPanel extends JPanel  {
 
+    private JPanel sliderPanel;
+
+
     public RightPanel(){
 
-        JSlider sliderRed = new JSlider(0,255,125);
-        sliderRed.setName("Red");
+        sliderPanel = new JPanel();
+        sliderPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 
+        JSlider slider = new JSlider();
+        addSlider(slider, "Test");
+        add(sliderPanel, BorderLayout.NORTH);
+
+        slider = new JSlider();
+                addSlider(slider, "Test2");
+    //    add(sliderPanel, BorderLayout.CENTER);
+
+ /*       JSlider sliderRed = new JSlider(0,255,125);
+        sliderRed.setName("Red");
         sliderRed.setMajorTickSpacing(25);
         sliderRed.setPaintTicks(true);
         sliderRed.setPaintLabels(true);
@@ -33,9 +46,27 @@ public class RightPanel extends JPanel  {
 
         add(sliderRed, BorderLayout.NORTH);
         add(sliderGreen, BorderLayout.CENTER);
-        add(sliderBlue, BorderLayout.SOUTH);
+        add(sliderBlue, BorderLayout.SOUTH);*/
+
+
+
 
     }
+    public void addSlider(JSlider slider, String description){
+
+        slider.setMajorTickSpacing(25);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        slider.addChangeListener(MainWindow.mainwindow);
+
+        JPanel panel = new JPanel();
+        panel.add(slider);
+        panel.add(new JLabel(description));
+        sliderPanel.add(panel);
+
+
+    }
+
 
 
 
