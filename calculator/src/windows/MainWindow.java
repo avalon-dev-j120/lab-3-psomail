4535a7e86109e7d2244b9af0362def3c4b3f33da
Sergei Perminov <psomail@yandex.ru>
2019-12-03 22:54:53 +0300

    added buttons

--- calculator/src/windows/MainWindow.java
+++ calculator/src/windows/MainWindow.java
@@ -45,20 +45,45 @@ public class MainWindow extends JFrame implements ChangeListener {
 
         JPanel buttons = new JPanel();
         buttons.setLayout(new GridLayout(4,4));
-
-
-
-
+        button0 = new JButton("0");
+        button0.setBorder((BorderFactory.createEmptyBorder(3, 3, 3, 3)));
+        button1 = new JButton("1");
+        button2 = new JButton("2");
+        button3 = new JButton("3");
+        button4 = new JButton("4");
+        button5 = new JButton("5");
+        button6 = new JButton("6");
+        button7 = new JButton("7");
+        button8 = new JButton("8");
+        button9 = new JButton("9");
+        buttonCE = new JButton("CE");
+        buttonPlus = new JButton("+");
+        buttonMinus = new JButton("-");
+        buttonDev = new JButton("*");
+        buttonSplitUp = new JButton("/");
+        buttonPoint = new JButton(".");
+        setLayout(new BorderLayout());
+        buttons.add(button7,BorderLayout.CENTER);
+        buttons.add(button8,BorderLayout.CENTER);
+        buttons.add(button9,BorderLayout.CENTER);
+        buttons.add(buttonPlus,BorderLayout.CENTER);
+        buttons.add(button4,BorderLayout.CENTER);
+        buttons.add(button5,BorderLayout.CENTER);
+        buttons.add(button6,BorderLayout.CENTER);
+        buttons.add(buttonMinus,BorderLayout.CENTER);
+        buttons.add(button1,BorderLayout.CENTER);
+        buttons.add(button2,BorderLayout.CENTER);
+        buttons.add(button3,BorderLayout.CENTER);
+        buttons.add(buttonDev,BorderLayout.CENTER);
+        buttons.add(buttonCE,BorderLayout.CENTER);
+        buttons.add(button0,BorderLayout.CENTER);
+        buttons.add(buttonPoint,BorderLayout.CENTER);
+        buttons.add(buttonSplitUp,BorderLayout.CENTER);
 
         JPanel buttonCalc = new JPanel();
         buttonCalculate = new JButton("=");
         buttonCalc.setLayout(new BorderLayout());
         buttonCalc.add(buttonCalculate, BorderLayout.CENTER);
-
-
-
-
-
 
         setLayout(new BorderLayout());
         add(display, BorderLayout.NORTH);
